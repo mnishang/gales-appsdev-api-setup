6f9aed548a1a16a0a61a11ac45a7671bff4627cd
shanggwapa <bastyshane1325@gmail.com>
2026-09-18 23:43:24 +0800

    Complete API setup

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,27 +1,34 @@
-import js from "@eslint/js";
+/// <reference types="node" />
+import eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
+  globalIgnores([
+    "dist/**",
+    "node_modules/**",
+    "src/generated/**",
+    "src/prisma/contract.d.ts",
+    "eslint.config.js",
+    "eslint.config.ts",
+    "prisma.config.ts",
+    ".env",
+    "*.sql",
+    "*.md",
+  ]),
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
   {
-    ignores: [
-      "node_modules/**",
-      "dist/**",
-      "src/prisma/contract.d.ts",
-    ],
-  },
-
-  js.configs.recommended,
-
-  ...tseslint.configs.recommended,
-
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          argsIgnorePattern: "^_",
-        },
-      ],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-  },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-console": "off",
+    },
+  }
 );
